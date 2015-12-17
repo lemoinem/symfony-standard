352564e6e534df8429928ca058a1ca922b8bd63f
Mathieu Lemoine <mathieu@wemakecustom.com>
2015-12-17 14:09:34 -0500

    Test for MappedSuperclass Repository

---
+++ src/AppBundle/Command/TestCommand.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace AppBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class FooCommand extends ContainerAwareCommand
+{
+    protected function configure()
+    {
+        $this->setName('test');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        var_dump($this->getContainer()->get('doctrine.orm.entity_manager')->getRepository('AppBundle:Mapped')->findAll());
+    }
+}
