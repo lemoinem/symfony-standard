1ce09396e4da157b7a131cdd4b2e8ba243c994a9
Mathieu Lemoine <mathieu@wemakecustom.com>
2015-12-17 14:09:34 -0500

    Test for MappedSuperclass Repository

---
+++ src/AppBundle/Entity/Root.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\InheritanceType("SINGLE_TABLE")
+ * @ORM\DiscriminatorColumn(name="discr", type="string")
+ * @ORM\DiscriminatorMap({"root" = "Root", "entity" = "Entity"})
+ */
+class Root
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    protected $string;
+}
